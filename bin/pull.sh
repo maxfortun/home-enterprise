739045029be18b185adcbab09c2206fee42a53c3
Maxim Fortun <max@maxf.net>
2020-10-18 09:25:28 -0400

    Added

---
+++ bin/pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+cd $(dirname $0)/..
+
+[ -d repo ] || mkdir repo
+cd repo
+
+while read repo; do
+	[ -d "${repo}" ] && pushd "$repo" && git pull && popd
+done <<_EOT_
+	docker-sshd
+	docker-lsyncd
+_EOT_
