d9beb460bc05f4f485e3c89485dc213509cdf351
Max Fortun <max@maxf.net>
2021-04-18 09:05:50 -0400

    Added

--- bin/ssh-keys-to-dest.sh
+++ bin/ssh-keys-to-dest.sh
@@ -2,4 +2,11 @@
 host=$1
 port=$2
 guest=$3
-scp -P $port mnt/$(hostname -s)/root/.ssh/id_rsa.pub $host:home-enterprise/mnt/$guest/root/.ssh/authorized-keys
+
+if [ -z "$guest" ]; then
+	echo "Usage: $0 <host> <port> <local hostname>"
+	echo "e.g.: $0 server.example.com 10222 server"
+	exit 1
+fi
+
+scp -P $port mnt/$(hostname -s)/root/.ssh/id_rsa.pub $host:home-enterprise/mnt/$guest/root/.ssh/authorized_keys
